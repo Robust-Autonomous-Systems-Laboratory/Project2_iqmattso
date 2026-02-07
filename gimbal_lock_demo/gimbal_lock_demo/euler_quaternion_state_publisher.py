cf132bee3576133aede42f5fb3c33a3246a7860f
Ian Q. Mattson <iqmattso@mtu.edu>
2026-02-06 23:31:26 -0500

    demo is working, rviz opens with launch file, rqt_reconfigure works and state changes are reflected in rviz

--- gimbal_lock_demo/gimbal_lock_demo/euler_quaternion_state_publisher.py
+++ gimbal_lock_demo/gimbal_lock_demo/euler_quaternion_state_publisher.py
@@ -1,10 +1,9 @@
-from math import sin, cos, pi
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-from geometry_msgs.msg import Quaternion
+from scipy.spatial.transform import Rotation as R
 from sensor_msgs.msg import JointState
-from tf2_ros import TransformBroadcaster, TransformStamped
+from tf2_ros import TransformBroadcaster
 from rcl_interfaces.msg import ParameterDescriptor, FloatingPointRange, SetParametersResult
 
 class SliderNode(Node):
@@ -17,13 +16,6 @@ class SliderNode(Node):
         self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
         self.nodeName = self.get_name()
 
-        # message declarations
-        odom_trans = TransformStamped()
-        odom_trans.header.frame_id = 'odom'
-        odom_trans.child_frame_id = 'axis'
-        joint_state = JointState()
-
-
         param_range = FloatingPointRange(from_value=-360.0, to_value=360.0, step=0.0)
         param_descriptor = ParameterDescriptor(floating_point_range=[param_range])
 
@@ -31,14 +23,19 @@ class SliderNode(Node):
         self.declare_parameter('pitch', 0.0, param_descriptor)
         self.declare_parameter('roll', 0.0, param_descriptor)
         self.declare_parameter('use_quaternions', False)
-        self.use_quaternions = False
         self.add_on_set_parameters_callback(self.slider_callback)
+
+        self.use_quaternions = False
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0
 
         self.get_logger().info("Node Started, open rqt_reconfigure")
 
 
-
     def slider_callback(self, params):
+
+        # first, update params
         for param in params:
 
             if param.name == 'use_quaternions':
@@ -47,23 +44,35 @@ class SliderNode(Node):
 
             if param.name == 'roll':
                 self.get_logger().info(f"Roll: {param.value}")
+                self.roll = param.value
 
             if param.name == 'pitch':
                 self.get_logger().info(f"Pitch: {param.value}")
+                self.pitch = param.value
 
             if param.name == 'yaw':
                 self.get_logger().info(f"Yaw: {param.value}")
+                self.yaw = param.value
+
+        # then, populate and publish a JointState() msg
+        msg = JointState()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.name = ['roll_joint', 'pitch_joint', 'yaw_joint']
+        
+
+        if self.use_quaternions:
+            # directly control euler angles
+            msg.position = [self.roll, self.pitch, self.yaw]
+
+        else:
+            # convert from euler to quaternions
+            quaternion_rotation = R.from_euler('zyx', [self.roll, self.pitch, self.yaw])
+            r, p, y = quaternion_rotation.as_euler('xyz')
+            msg.position = [r, p, y]
+
+        self.joint_pub.publish(msg)
 
         return SetParametersResult(successful=True)
-
-
-
-def euler_to_quaternion(roll, pitch, yaw):
-    qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
-    qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
-    qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
-    qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
-    return Quaternion(x=qx, y=qy, z=qz, w=qw)
 
 def main():
     rclpy.init()
